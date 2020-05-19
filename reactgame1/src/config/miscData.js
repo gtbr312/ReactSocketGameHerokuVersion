6d77125dc0bebf15ebf5a736f5ccc44a5a50abcb
Matt <nostalgiahog@gmail.com>
2020-05-18 20:21:33 -0700

    initial commit port check

--- reactgame1/src/config/miscData.js
+++ reactgame1/src/config/miscData.js
@@ -2,4 +2,4 @@ export const TILE_SIZE = Math.floor((window.innerWidth)/20)
 export const PLAYER_SPRITE_SIZE = TILE_SIZE
 export const MAP_HEIGHT = PLAYER_SPRITE_SIZE * 10
 export const MAP_WIDTH = PLAYER_SPRITE_SIZE * 20
-export const ENDPOINT = process.env.PORT || 'localhost:5000'
+export const ENDPOINT = process.env.PORT || 'localhost:3000'
