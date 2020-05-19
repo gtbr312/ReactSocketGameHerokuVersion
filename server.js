deed2c3fcf01b496882d94c1edab49abf6c9b968
Matt <nostalgiahog@gmail.com>
2020-05-18 20:35:41 -0700

    fixed export

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
-export const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express()
 const router = require('./router')
